3b1ea7aa585a7897cb9815b4a98578bbadebe5b0
riquetti <lfriquetti@hotmail.com>
2024-05-12 15:08:28 -0300

    Projeto Orquestra Sinfonica - commit 7
    Atualização de correção para deploy Vercell

--- biblioteca/settings.py
+++ biblioteca/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-51%05e2*phpmn9mj$epml3mk14rgrigvffc(g3#g0y^90ywv**
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 #ALLOWED_HOSTS = []
 
 
